225b6aebc71a0776513dbcd34a2c17f767063e6a
Pedro95DH <pedrosdhdez@gmail.com>
2024-10-04 10:42:35 +0100

    .
    .

--- ejercicio4.js
+++ ejercicio4.js
@@ -3,17 +3,31 @@
 // Imprime el nombre por consola.
 
 const cartoons = [
-    { name: 'Mickey Mouse', debut: 1928 },
-    { name: 'Bugs Bunny', debut: 1938 },
-    { name: 'SpongeBob SquarePants', debut: 1999 },
-    { name: 'Tom and Jerry', debut: 1940 },
-    { name: 'Scooby-Doo', debut: 1969 },
-    { name: 'The Flintstones', debut: 1960 },
-    { name: 'Batman: The Animated Series', debut: 1992 },
-    { name: 'The Simpsons', debut: 1989 },
-    { name: 'Pokémon', debut: 1997 },
-    { name: "Dexter's Laboratory", debut: 1996 }
-  ];
-  
-  // Añade tu código de bucle aquí
-  
+  { name: "Mickey Mouse", debut: 1928 },
+  { name: "Bugs Bunny", debut: 1938 },
+  { name: "SpongeBob SquarePants", debut: 1999 },
+  { name: "Tom and Jerry", debut: 1940 },
+  { name: "Scooby-Doo", debut: 1969 },
+  { name: "The Flintstones", debut: 1960 },
+  { name: "Batman: The Animated Series", debut: 1992 },
+  { name: "The Simpsons", debut: 1989 },
+  { name: "Pokémon", debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 },
+];
+
+// Añade tu código de bucle aquí
+let year = 0;
+let nombre = "";
+
+for (const cartoon of cartoons) {
+  if (year == 0) {
+    year = cartoon.debut;
+  } else {
+    console.log("pepino");
+    if (year > cartoon.debut) {
+      nombre = cartoon.name;
+    }
+  }
+}
+
+console.log(nombre);
