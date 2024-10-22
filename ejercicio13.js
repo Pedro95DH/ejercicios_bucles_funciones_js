e12f4efa1bb106e7003d3320de02ee5f2cb18c88
Pedro95DH <pedrosdhdez@gmail.com>
2024-10-22 14:39:18 +0100

    .
    .

--- ejercicio13.js
+++ ejercicio13.js
@@ -3,28 +3,29 @@
 // Averigua como hallar el año actual en tus cálculos.
 
 const actors = [
-  { name: 'Leonardo DiCaprio', born: 1974 },
-  { name: 'Tom Hanks', born: 1956 },
-  { name: 'Meryl Streep', born: 1949 },
-  { name: 'Brad Pitt', born: 1963 },
-  { name: 'Johnny Depp', born: 1963 },
-  { name: 'Scarlett Johansson', born: 1984 },
-  { name: 'Jennifer Lawrence', born: 1990 },
-  { name: 'Denzel Washington', born: 1954 },
-  { name: 'Morgan Freeman', born: 1937 },
-  { name: 'Cate Blanchett', born: 1969 }
+  { name: "Leonardo DiCaprio", born: 1974 },
+  { name: "Tom Hanks", born: 1956 },
+  { name: "Meryl Streep", born: 1949 },
+  { name: "Brad Pitt", born: 1963 },
+  { name: "Johnny Depp", born: 1963 },
+  { name: "Scarlett Johansson", born: 1984 },
+  { name: "Jennifer Lawrence", born: 1990 },
+  { name: "Denzel Washington", born: 1954 },
+  { name: "Morgan Freeman", born: 1937 },
+  { name: "Cate Blanchett", born: 1969 },
 ];
 
 function calculateActorsAges(actors) {
-  let fecha = new Date();
-  let year = fecha.getFullYear();
-  const ageActors = [{"nombre":null,"age":null}];
-  
+  const ageActors = [];
   for (let i = 0; i < actors.length; i++) {
-    ageActors.push(nombre[actors[i].name], age[year - actors[i].born]);
+    ageActors.push({
+      name: actors[i].name,
+      age: new Date().getFullYear() - actors[i].born,
+    });
   }
   console.log(ageActors);
 }
 
-
 calculateActorsAges(actors);
+
+
